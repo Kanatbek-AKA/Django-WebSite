738c9c2ce987914800fd03a76502669db03f4cda
Kanatbek-AKA <61031156+Kanatbek-AKA@users.noreply.github.com>
2023-03-10 22:10:35 +0100

    Update drop_all_tables_one_go.sql

--- drop_all_tables_one_go.sql
+++ drop_all_tables_one_go.sql
@@ -1,8 +1,8 @@
 -- Drop all tables by one go
--- PRAGMA writable_schema = 1;
--- DELETE FROM sqlite_master;
--- PRAGMA writable_schema = 0;
--- VACUUM;
--- PRAGMA integrity_check;
-
--- select * from sqlite_schema;
+PRAGMA writable_schema = 1;
+DELETE FROM sqlite_master;
+PRAGMA writable_schema = 0;
+VACUUM;
+PRAGMA integrity_check;
+-- Checking removal of tables 
+select * from sqlite_schema;
